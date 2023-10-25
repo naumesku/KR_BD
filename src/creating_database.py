9e33eb80616a5340c4bd5be4d6ff1d6df2a9b220
Dmitriy Naymov <prp4@bk.ru>
2023-10-25 21:40:58 +0300

    fix drop db

--- src/creating_database.py
+++ src/creating_database.py
@@ -18,10 +18,11 @@ class CREATE_DB():
 
         with conn.cursor() as cur:
             conn.autocommit = True
-            try:
-                cur.execute(f"DROP DATABASE {self.database_name}")
-            except psycopg2.errors.InvalidCatalogName:
-                pass
+            # try:
+            #     cur.execute(f"DROP DATABASE {self.database_name}")
+            # except psycopg2.errors.InvalidCatalogName:
+            #     pass
+            cur.execute(f"DROP DATABASE IF EXISTS {self.database_name}")
             cur.execute(f"CREATE DATABASE {self.database_name}")
             conn.commit()
         conn.close()
